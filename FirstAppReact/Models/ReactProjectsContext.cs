56c395095847efa624ed86039b768d4abccf7906
Ivan Vazquez <xiao-jiisus@outlook.com>
2023-03-26 01:22:34 -0600

    Adding connection string

---
+++ FirstAppReact/Models/ReactProjectsContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstReact.Models;
+
+public partial class ReactProjectsContext : DbContext
+{
+    public ReactProjectsContext()
+    {
+    }
+
+    public ReactProjectsContext(DbContextOptions<ReactProjectsContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Tarea> Tareas { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=localhost; Initial Catalog=ReactProjects; User ID=SA; Password=yourPassword; MultipleActiveResultSets=False; TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Tarea>(entity =>
+        {
+            entity.HasKey(e => e.IdTarea).HasName("PK__Tarea__EADE909893680892");
+
+            entity.ToTable("Tarea");
+
+            entity.Property(e => e.Descripcion)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.FechaRegistro)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
